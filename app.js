8ecc0af1adf6d5d27c071cccca5570361318f6a4
nadiyaheb <nadiyaheb@gmail.com>
2016-04-11 19:37:07 -0300

    presentation time

--- app.js
+++ app.js
@@ -4,21 +4,25 @@ var fireb = new Firebase("https://getcookin.firebaseio.com");
 var ingredients =[];
 var recipeListArr = [];
 
-var ingredientListHtml = $('#ingreList').html();
-var compiledTemplateList = Handlebars.compile(ingredientListHtml);
+var pantryListHtml = $('#ingreList').html();
+var compiledTemplateList = Handlebars.compile(pantryListHtml);
 
 var suggestedRecipesHtml = $('#recipeList').html();
 var compiledRecipeTemplateList = Handlebars.compile(suggestedRecipesHtml);
 
-var featureRecipesHtml = $('#featuredRecipe').html();
-var compliedFeatureRecipeTemplate = Handlebars.compile(featureRecipesHtml);
+var featuredRecipesHtml = $('#featuredRecipe').html();
+var compliedFeatureRecipeTemplate = Handlebars.compile(featuredRecipesHtml);
+
+var ingredientsListHtml = $('#featuredIngredientList').html();
+var compliedIngredientsListTemplate = Handlebars.compile(ingredientsListHtml);
 
 
 $("#addToPantry").submit(function(e){
+	//Creates the user ingredient list based on the users input
 	e.preventDefault();
 	var $foodItem = $(this).find('input[name="foodItem"]');
-	//console.log($foodItem.val());
-
+	
+	//Users input is saved to firebase database
 	fireb.child("foodItem").push({
 		name:$foodItem.val(),
 		amount: 1,
@@ -28,14 +32,15 @@ $("#addToPantry").submit(function(e){
 })
 
 function getPantry(){
+	//Creates the user ingredient list based on the contents grabbed from firebase that user inputs
+
 	fireb.child("foodItem").on('value', function(results){
-		$("#pantryInventory").empty();
+		$("#pantryList").empty();
 		ingredients =[];
 		var values = results.val();
 		
 		for(var key in values){
 
-			//console.log(values[key]);
 			var food = values[key];
 
 			ingredients.push(food);
@@ -45,14 +50,15 @@ function getPantry(){
 				key: key
 			})
 			
-			$("#pantryInventory").append(compliedHtml);
+			$("#pantryList").append(compliedHtml);
 		}
 			
 		
 	})
 }
 
-$('#pantryInventory').on("click", ".delete-btn",function(e){
+$('#pantryList').on("click", ".delete-btn",function(e){
+	//removes the ingredient from the pantry list
 	e.preventDefault;
 	var food = $(this).data("id");
 	removeFoodItem(food);
@@ -60,50 +66,37 @@ $('#pantryInventory').on("click", ".delete-btn",function(e){
 })
 
 $("#getRecipes").on("click", function(){
+	
+	// Creates search query and returns the results of the search
 	var yumID = "85a16a2a";
 	var yumKey = "4dce213045767f5ce8d95fd7bcf16500";
 	var foodQuery = "requirePictures=true";
 	var ingredientParam = "";
 	$("#suggestedRecipes").empty();
-	recipeListArr=[];
 	console.log(ingredients);
-	//console.log("recipeListArr",recipeListArr);
+
 	ingredients.forEach(function(ingredient){
 		var addToList = "allowedIngredient[]=" + ingredient.name +"&";
 
 		ingredientParam += addToList;
 	})
 
-	//console.log(ingredientParam);
 
-	//foodQuery += ingredientParam;
-
-	var url = "http://api.yummly.com/v1/api/recipes?_app_id="+ yumID +"&_app_key="+ yumKey +"&" + ingredientParam + foodQuery + "&maxResult=20&start=20";
+	var url = "http://api.yummly.com/v1/api/recipes?_app_id="+ yumID +"&_app_key="+ yumKey +"&" + ingredientParam + foodQuery + "&maxResult=10&start=20";
 	
 	$.ajax({
 		url: url,
 		method: "GET",
 		dataType: "jsonp",
 		success: function(response){
-			//console.log(url);
-			//console.log(response);
+
 			var matches = response.matches;
-			//console.log(matches);
+			console.log(matches);
 			matches.forEach(function(foo){
 
-				//var recipeName = foo.recipeName;
-				//console.log(recipeName);
 				var orginalImgUrl = foo.imageUrlsBySize[90];
 				var imgUrl = orginalImgUrl.replace("=s90-c","=s480-c-e365");
 				var totalCookTime = (foo.totalTimeInSeconds/60);
-
-				recipeListArr.push({
-					recipeName: foo.recipeName,
-					imgUrl: imgUrl,
-					id: foo.id,
-					totalCookTime: totalCookTime
-
-				});
 
 				var compliedRecipeHtml = compiledRecipeTemplateList({
 					recipeName: foo.recipeName,
@@ -112,7 +105,7 @@ $("#getRecipes").on("click", function(){
 					totalCookTime: totalCookTime
 
 				})
-
+				
 				$("#suggestedRecipes").append(compliedRecipeHtml);
 			})
 
@@ -120,8 +113,13 @@ $("#getRecipes").on("click", function(){
 	})
 })
 
+
+
 $("#suggestedRecipes").on("click", ".recipeItem", function(e){
+	//Gets the details of the selected recipe
+
 	$("#mainRecipe").empty();
+	$("#ingredientList").removeClass("hide");
 	e.preventDefault();
 
 	var yumID = "85a16a2a";
@@ -137,17 +135,31 @@ $("#suggestedRecipes").on("click", ".recipeItem", function(e){
 		dataType: "jsonp",
 		success: function(response){
 			var source = response.source;
-			var RecipeUrl = source.sourceRecipeUrl;
-			var RecipeSource = source.sourceDisplayName;
+			var recipeUrl = source.sourceRecipeUrl;
+			var recipeSource = source.sourceDisplayName;
 			var img = response.images;
 			var serving = response.numberOfServings;
 			var totalCookTime = (response.totalTimeInSeconds/60);
 			var prepTime = (response.prepTimeInSeconds/60);
-			
+			var ingredientItem = [];
+			var ingredientLines = response.ingredientLines;
 			console.log(response);
 			console.log(response.prepTime);
-			
+			$("#recipeIngredientList").empty();
+			ingredientLines.forEach(function(item){
 
+				ingredientItem.push(item);
+				var ingredientItems = $("<li>" + item + "</li>")
+			
+			var compliedIngredientsHtml = compliedIngredientsListTemplate({
+					item: ingredientItems.html()	
+				})
+
+			$("#recipeIngredientList").append(compliedIngredientsHtml);
+			
+			});
+
+			console.log(img[0].hostedLargeUrl);
 			var compliedFeatureRecipeHtml = compliedFeatureRecipeTemplate({
 				recipeName: response.name,
 				imgUrl: img[0].hostedLargeUrl,
@@ -155,14 +167,15 @@ $("#suggestedRecipes").on("click", ".recipeItem", function(e){
 				totalCookTime: totalCookTime,
 				numberOfServings: response.numberOfServings,
 				prepTime: prepTime,
-				source: RecipeSource
+				source: recipeSource,
+				sourceUrl: recipeUrl
 
 			})
-
-
+	
 			$("#mainRecipe").append(compliedFeatureRecipeHtml);
-			
+			$("#ingredientList").removeClass("hide");
 
+			
 		}
 	})
 
